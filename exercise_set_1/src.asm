8c3040abc0315a9756cafb351a1af7031c0f4057
Sam Johnson <sambjohnson94@gmail.com>
2022-11-23 20:32:15 -0800

    ex6, rm cart file

--- exercise_set_1/src.asm
+++ exercise_set_1/src.asm
@@ -6,26 +6,26 @@
 Start:
     sei
     cld
-    ; Load the A register with the hexadecimal value $A
-    ; Load the X register with the binary value %1010
-    lda #$A
-    ldx #%1010
+    ; Load the A register with the decimal value 1
+    ; Load the X register with the decimal value 2
+    ; Load the Y register with the decimal value 3
+    lda #1
+    ldx #2
+    ldy #3
 
-    ; Store the value in the A register into (zero page) memory address $80
-    ; Store the value in the X register into (zero page) memory address $81
-    sta $80
-    stx $81
+    ; Increment X
+    ; Increment Y
+    ; Increment A
+    inx
+    iny
+    adc #0
 
-    ; Load A with the decimal value 10
-    ; Add to A the value inside RAM address $80
-    ; Add to A the value inside RAM address $81
-    ; A should contain (#10 + $A + %1010) = #30 (or $1E in hexadecimal)
-    lda #10
-    adc $80
-    adc $81
-
-    ; Store the value of A into RAM position $82
-    sta $82
+    ; Decrement X
+    ; Decrement Y
+    ; Decrement A
+    dex
+    dey
+    sbc #0
 
     jmp Start
 
