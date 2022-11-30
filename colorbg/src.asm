947da61196e04351086793e28e7c7a9c220ab67a
Sam Johnson <sambjohnson94@gmail.com>
2022-11-30 15:34:05 -0800

    add clean_start and colubk to colorbg

--- colorbg/src.asm
+++ colorbg/src.asm
@@ -1,10 +1,21 @@
     processor 6502
+
     include "vcs.h"
     include "macro.h"
+
     seg Code
     org $F000
 
 Start:
+;    CLEAN_START                 ; Macro to safely clean memory
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Set BG Luminosity to Yellow
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #$1E                    ; 1,14 NTSC is a bright yellow on the 2600
+    sta COLUBK                  ; Store in the address for background luminance
+
+    jmp Start
 
     org $FFFC
     .word Start
